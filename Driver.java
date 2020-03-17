fcf1c47cd0d555882c07343249f320291261133c
sunnyjovita <55756082+sunnyjovita@users.noreply.github.com>
2020-03-17 23:49:06 +0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -9,31 +9,31 @@ public class Driver {
         Utility u = new Utility();
 
         // exercise 1
-//        int a[] = {2,8,6,3,2};
-//        System.out.println("Before" + Arrays.toString(a));
-//        int num = u.split(a);
+       int a[] = {2,8,6,3,2};
+       System.out.println("Before" + Arrays.toString(a));
+       int num = u.split(a);
 
 //        // exercise 2
-//        int arr2[] = {1, 2, 3, 4, 5};
-//        System.out.println("Before: " + Arrays.toString(arr2));
-//        int num2 = u.getRot(arr2);
+       int arr2[] = {1, 2, 3, 4, 5};
+       System.out.println("Before: " + Arrays.toString(arr2));
+       int num2 = u.getRot(arr2);
 
 //        // exercise 3
-//        int matrix[][] = {{1, 2, 3},
-//                {4, 5, 6},
-//                {7, 8, 9}};
-//        if (u.isSymmetric(matrix, 3)) {
-//            System.out.println("Yes, the matrix is symmetric");
-//        } else {
-//
-//            System.out.println("NO");
-//        }
+       int matrix[][] = {{1, 2, 3},
+               {4, 5, 6},
+               {7, 8, 9}};
+       if (u.isSymmetric(matrix, 3)) {
+           System.out.println("Yes, the matrix is symmetric");
+       } else {
+
+           System.out.println("NO");
+       }
 
         // exercise 4
         // magic square for odd number
-//        u.checkEven();
+       u.checkEven();
         // magic square for even number
-//        u.checkOdd();
+       u.checkOdd();
 
     }
 }
